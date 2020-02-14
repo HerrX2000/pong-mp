2e46cee1a2afefbf53caaea3e52149371a364c70
Freddy@Desktop <youfreddi@gmail.com>
2020-02-14 13:44:45 +0100

    added connecting code

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -7,6 +7,7 @@ var score={}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pre_configure_game();
 	$goal_1.connect("body_entered", self, "on_body_entered", [2])
 	$goal_2.connect("body_entered", self, "on_body_entered", [1])
 	if GLOBAL.game_nmb!=0:
@@ -23,6 +24,25 @@ func on_body_entered(body,player) -> void:
 # Called every fram
 func _process(delta):
 	pass
+
+remote func pre_configure_game():
+	var selfPeerID = get_tree().get_network_unique_id()
+
+	# Load my player
+	var my_player = preload("res://objects/player.tscn").instance()
+	player1.set_name(str(selfPeerID))
+	player1.set_network_master(selfPeerID) # Will be explained later
+	get_node("/root/world/players").add_child(my_player)
+
+	# Load other players
+	for p in player_info:
+		var player2 = preload("res://objects/player.tscn").instance()
+		player2.set_name(str(p))
+		player2.set_network_master(p) # Will be explained later
+		get_node("/root/world/players").add_child(player)
+
+	# Tell server (remember, server is always ID=1) that this peer is done pre-configuring.
+	rpc_id(1, "done_preconfiguring", selfPeerID)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
